33c67463a0434a54a7f7fcd703730fa34f753a26
Fedor Noskov <fedor.noskov.99@mail.ru>
2018-09-10 20:42:07 +0300

    misprint

--- points_generator.h
+++ points_generator.h
@@ -7,7 +7,7 @@ using point = pair<float, float>;
 class points_generator {
 public:
 	//генерирует 1 точку на плоскости с нормальным распределением по обеим ос¤м
-	//использу¤ метод Сокса-Мюллера
+	//использу¤ метод Бокса-Мюллера
 	pair<float, float> Box_Muller();
 
 	//генерирует N точек
